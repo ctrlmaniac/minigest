2c5b4a2e934fd217916b6fa0fa7a4f0c935b8852
Davide Di Criscito <davide.dicriscito@gmail.com>
2022-12-13 16:30:33 +0100

    add all CRUD methods for User model

--- src/main/java/me/ctrlmaniac/minigest/controllers/UserController.java
+++ src/main/java/me/ctrlmaniac/minigest/controllers/UserController.java
@@ -5,7 +5,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +27,27 @@ public class UserController {
     @GetMapping("")
     public ResponseEntity<List<User>> getAllUsers() {
         return new ResponseEntity<>(userService.getAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{email}")
+    public ResponseEntity<User> getUserByEmail(@PathVariable String email) {
+        return new ResponseEntity<>(userService.getByEmail(email), HttpStatus.OK);
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        userService.createUser(user);
+        return new ResponseEntity<>(user, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/delete/{email}")
+    public ResponseEntity<String> deleteUserByEmail(@PathVariable String email) {
+        userService.deleteUser(email);
+        return new ResponseEntity<>("Utente cancellato con successo!", HttpStatus.OK);
+    }
+
+    @PutMapping("/update/{email}")
+    public ResponseEntity<User> updateUserByEmail(@PathVariable String email, @RequestBody User updatedUser) {
+        return new ResponseEntity<>(userService.updateUser(email, updatedUser), HttpStatus.OK);
     }
 }
--- src/main/java/me/ctrlmaniac/minigest/services/UserService.java
+++ src/main/java/me/ctrlmaniac/minigest/services/UserService.java
@@ -1,6 +1,7 @@
 package me.ctrlmaniac.minigest.services;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
@@ -19,5 +20,45 @@ public class UserService {
 
     public List<User> getAll() {
         return users;
+    }
+
+    public User getByEmail(String email) {
+        for (User user : users) {
+            if (user.getEmail().equals(email)) {
+                return user;
+            }
+        }
+
+        return null;
+    }
+
+    public User createUser(User user) {
+        users.add(user);
+
+        return user;
+    }
+
+    public void deleteUser(String email) {
+        Iterator<User> userIterator = users.iterator();
+
+        while (userIterator.hasNext()) {
+            User tempUser = userIterator.next();
+
+            if (tempUser.getEmail().equals(email)) {
+                userIterator.remove();
+            }
+        }
+    }
+
+    public User updateUser(String email, User updatedUser) {
+        for (User user : users) {
+            if (user.getEmail().equals(email)) {
+                user.setEmail(updatedUser.getEmail());
+                user.setName(updatedUser.getName());
+                user.setSurname(updatedUser.getSurname());
+            }
+        }
+
+        return updatedUser;
     }
 }
