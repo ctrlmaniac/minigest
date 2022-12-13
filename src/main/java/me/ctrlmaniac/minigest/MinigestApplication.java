2a0f8cff503cb6a69c381b03ab4045b739c8b437
Davide Di Criscito <davide.dicriscito@gmail.com>
2022-12-13 15:18:32 +0100

    init spring boot

---
+++ src/main/java/me/ctrlmaniac/minigest/MinigestApplication.java
@@ -0,0 +1,13 @@
+package me.ctrlmaniac.minigest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinigestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinigestApplication.class, args);
+	}
+
+}
