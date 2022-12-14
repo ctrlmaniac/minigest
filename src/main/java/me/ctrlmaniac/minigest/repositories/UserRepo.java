2cc0b99fcafb135108c13002d37e368eff02e076
Davide Di Criscito <davide.dicriscito@gmail.com>
2022-12-14 14:03:05 +0100

    add UserRepo

---
+++ src/main/java/me/ctrlmaniac/minigest/repositories/UserRepo.java
@@ -0,0 +1,9 @@
+package me.ctrlmaniac.minigest.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import me.ctrlmaniac.minigest.entitities.User;
+
+public interface UserRepo extends JpaRepository<User, Long> {
+
+}
