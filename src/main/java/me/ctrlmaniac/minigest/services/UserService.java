124b5b1ac0881c645e2e4f0cea5af719e2bf2094
Davide Di Criscito <davide.dicriscito@gmail.com>
2022-12-14 14:05:23 +0100

    update UserService

--- src/main/java/me/ctrlmaniac/minigest/services/UserService.java
+++ src/main/java/me/ctrlmaniac/minigest/services/UserService.java
@@ -4,61 +4,23 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import me.ctrlmaniac.minigest.entitities.User;
+import me.ctrlmaniac.minigest.repositories.UserRepo;
 
 @Service
 public class UserService {
-    List<User> users;
 
-    public UserService() {
-        users = new ArrayList<>();
+    @Autowired
+    UserRepo userRepo;
 
-        users.add(new User("davide.dicriscito@gmail.com", "Davide", "Di Criscito"));
+    public User saveUser(User u) {
+        return userRepo.save(u);
     }
 
-    public List<User> getAll() {
-        return users;
-    }
-
-    public User getByEmail(String email) {
-        for (User user : users) {
-            if (user.getEmail().equals(email)) {
-                return user;
-            }
-        }
-
-        return null;
-    }
-
-    public User createUser(User user) {
-        users.add(user);
-
-        return user;
-    }
-
-    public void deleteUser(String email) {
-        Iterator<User> userIterator = users.iterator();
-
-        while (userIterator.hasNext()) {
-            User tempUser = userIterator.next();
-
-            if (tempUser.getEmail().equals(email)) {
-                userIterator.remove();
-            }
-        }
-    }
-
-    public User updateUser(String email, User updatedUser) {
-        for (User user : users) {
-            if (user.getEmail().equals(email)) {
-                user.setEmail(updatedUser.getEmail());
-                user.setName(updatedUser.getName());
-                user.setSurname(updatedUser.getSurname());
-            }
-        }
-
-        return updatedUser;
+    public List<User> getAllUsers() {
+        return userRepo.findAll();
     }
 }
