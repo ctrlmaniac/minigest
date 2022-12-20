9bd0d0e880a4494fe0e53cfeb8217e6dafcf725d
Davide Di Criscito <davide.dicriscito@gmail.com>
2022-12-20 14:28:37 +0100

    complete rest endpoints for azienda

--- src/main/java/me/ctrlmaniac/minigest/controllers/AziendaRestController.java
+++ src/main/java/me/ctrlmaniac/minigest/controllers/AziendaRestController.java
@@ -10,8 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 import me.ctrlmaniac.minigest.entitities.Azienda;
 import me.ctrlmaniac.minigest.services.AziendaService;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/api/aziende")
@@ -32,4 +37,20 @@ public class AziendaRestController {
         return new ResponseEntity<>(azienda, azienda == null ? HttpStatus.NOT_FOUND : HttpStatus.OK);
     }
 
+    @PostMapping("/create")
+    public ResponseEntity<Azienda> create(@RequestBody Azienda a) {
+        aziendaService.save(a);
+        return new ResponseEntity<Azienda>(a, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> deleteById(@PathVariable String id) {
+        aziendaService.deleteById(id);
+        return new ResponseEntity<>("Azienda cancellata con successo!", HttpStatus.OK);
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<Azienda> update(@PathVariable String id, @RequestBody Azienda a) {
+        return new ResponseEntity<>(aziendaService.update(id, a), HttpStatus.OK);
+    }
 }
