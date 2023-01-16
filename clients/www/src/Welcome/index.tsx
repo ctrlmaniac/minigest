77a04a5b2c340044815c261795ec159970663472
Davide Di Criscito <davide.dicriscito@gmail.com>
2023-01-16 21:33:53 +0100

    inizializza la homepage

--- clients/www/src/Welcome/index.tsx
+++ clients/www/src/Welcome/index.tsx
@@ -1,7 +1,59 @@
+import {
+  Box,
+  Button,
+  Container,
+  Paper,
+  TextField,
+  ThemeProvider,
+  Typography,
+} from "@mui/material";
 import React from "react";
+import { logoTheme } from "~/theme";
 
 const Welcome: React.FC = () => {
-  return <h1>Welcome</h1>;
+  return (
+    <>
+      <Container>
+        <Box mb={4}>
+          <ThemeProvider theme={logoTheme}>
+            <Typography variant="h1" component="h1" align="center" mt={3}>
+              minigest
+            </Typography>
+          </ThemeProvider>
+
+          <Typography variant="h6" component="p" align="center">
+            Un mini gestionale pensato per le piccole/medie imprese
+          </Typography>
+        </Box>
+      </Container>
+
+      <Container maxWidth="xs">
+        <Paper>
+          <Box p={2}>
+            <form action="" method="post">
+              <TextField
+                fullWidth
+                label="email"
+                name="username"
+                type="email"
+                margin="normal"
+              />
+              <TextField
+                fullWidth
+                name="password"
+                label="password"
+                type="password"
+                margin="normal"
+              />
+              <Button fullWidth variant="contained" type="submit">
+                accedi
+              </Button>
+            </form>
+          </Box>
+        </Paper>
+      </Container>
+    </>
+  );
 };
 
 export default Welcome;
--- clients/www/src/main.tsx
+++ clients/www/src/main.tsx
@@ -2,16 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Route, Routes } from "react-router";
 import { BrowserRouter } from "react-router-dom";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 import Welcome from "./Welcome";
 import App from "./App";
+import { theme } from "./theme";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Welcome />} />
-        <Route path="/dashboard" element={<App />} />
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Welcome />} />
+          <Route path="/dashboard" element={<App />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>
 );
