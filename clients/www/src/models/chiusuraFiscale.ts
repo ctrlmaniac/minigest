150cb7a694dcc030351dc5c619331256268d19f9
Davide Di Criscito <davide.dicriscito@gmail.com>
2023-01-17 21:25:50 +0100

    aggiunge i modelli per le chiusure fiscali

---
+++ clients/www/src/models/chiusuraFiscale.ts
@@ -0,0 +1,9 @@
+import ChiusuraFiscaleReparto from "./chiusuraFiscaleReparto";
+
+export default interface ChiusuraFiscale {
+  id: string;
+  data: string;
+  totale: number;
+  numeroDocFisc: number;
+  reparti: ChiusuraFiscaleReparto[];
+}
---
+++ clients/www/src/models/chiusuraFiscaleReparto.ts
@@ -0,0 +1,7 @@
+export default interface ChiusuraFiscaleReparto {
+  id: string;
+  aliquota: number;
+  totale: number;
+  totaleAnnulli: number;
+  totaleResi: number;
+}
