5209e0207b4817173ddab0acee508d3f93f06b17
Davide Di Criscito <davide.dicriscito@gmail.com>
2023-01-17 09:57:04 +0100

    elimina import non utilizzati

--- src/main/java/me/ctrlmaniac/minigest/controllers/rest/AccountController.java
+++ src/main/java/me/ctrlmaniac/minigest/controllers/rest/AccountController.java
@@ -3,12 +3,9 @@ package me.ctrlmaniac.minigest.controllers.rest;
 import java.security.Principal;
 import java.util.List;
 
-import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
