999fc452d41018fe8d351cb89c5b852e6be00973
Davide Di Criscito <davide.dicriscito@gmail.com>
2023-01-30 22:15:19 +0100

    inizializza la sidebar

---
+++ frontend/www/src/pages/App/Sidebar.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import {
+  Box,
+  Drawer,
+  List,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
+import { IconLayoutDashboard } from "@tabler/icons-react";
+
+interface Props {
+  open: boolean;
+  handleOpen: Function;
+}
+
+const Sidebar: React.FC<Props> = ({ open, handleOpen }) => {
+  const navigate = useNavigate();
+
+  const handleNavigate = (to: string) => {
+    navigate(`/app/${to}`);
+    handleOpen(false);
+  };
+
+  return (
+    <Drawer anchor="left" open={open} onClose={() => handleOpen(false)}>
+      <Box sx={{ width: 250 }}>
+        <List>
+          <ListItemButton onClick={() => handleNavigate("/")}>
+            <ListItemIcon>
+              <IconLayoutDashboard />
+            </ListItemIcon>
+            <ListItemText primary="Dashboard" />
+          </ListItemButton>
+        </List>
+      </Box>
+    </Drawer>
+  );
+};
+
+export default Sidebar;
--- frontend/www/src/pages/App/index.tsx
+++ frontend/www/src/pages/App/index.tsx
@@ -1,12 +1,16 @@
 import React from "react";
-import Navbar from "./Navbar";
 import { Outlet } from "react-router-dom";
 import { Container } from "@mui/material";
+import Navbar from "./Navbar";
+import Sidebar from "./Sidebar";
 
 const App: React.FC = () => {
+  const [openSidebar, setOpenSidebar] = React.useState(false);
+
   return (
     <>
-      <Navbar handleDrawerOpen={() => {}} />
+      <Navbar handleDrawerOpen={setOpenSidebar} />
+      <Sidebar open={openSidebar} handleOpen={setOpenSidebar} />
 
       <Container>
         <Outlet />
