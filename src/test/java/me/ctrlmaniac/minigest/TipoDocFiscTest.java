8b78fa16d72dd96d21ce7b958d9fa113bba045ef
Davide Di Criscito <davide.dicriscito@gmail.com>
2023-02-02 22:48:27 +0100

    aggiusta CSV e inizializza dei test

---
+++ src/test/java/me/ctrlmaniac/minigest/TipoDocFiscTest.java
@@ -0,0 +1,39 @@
+package me.ctrlmaniac.minigest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import me.ctrlmaniac.minigest.entitities.docfisc.TipoDocFisc;
+import me.ctrlmaniac.minigest.services.docfisc.TipoDocFiscService;
+
+@SpringBootTest
+public class TipoDocFiscTest {
+
+	@Autowired
+	TipoDocFiscService tipoDocFiscService;
+
+	/**
+	 * 
+	 */
+	@Test
+	void checkLoadFromCsv() {
+		List<TipoDocFisc> lista = tipoDocFiscService.getAll();
+
+		assertNotNull(lista);
+
+	}
+
+@Test
+	void checkCodice() {
+		TipoDocFisc TD24 = tipoDocFiscService.getByCodice("TD24");
+
+		assertNotNull(TD24);
+		assertEquals(TD24.getDescrizione(), "fattura differita di cui all'art. 21, comma 4, terzo periodo lett. a) DPR 633/72");
+	}
+}
