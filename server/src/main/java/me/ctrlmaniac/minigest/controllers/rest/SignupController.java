6a0d1bfb45a56fed6612a485dbcc0152a0812191
Davide Di Criscito <davide.dicriscito@gmail.com>
2023-02-05 17:30:56 +0100

    rimuove l'autowired

--- server/src/main/java/me/ctrlmaniac/minigest/controllers/rest/SignupController.java
+++ server/src/main/java/me/ctrlmaniac/minigest/controllers/rest/SignupController.java
@@ -6,7 +6,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -41,9 +40,6 @@ public class SignupController {
 
 	@Autowired
 	NegozioService negozioService;
-
-	@Autowired
-	AuthenticationManager authenticationManager;
 
 	@PostMapping("")
 	public ResponseEntity<String> register(@RequestBody FormRegistrazioneDTO form) {
