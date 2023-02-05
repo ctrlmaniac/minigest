11cba701d739a7d6694abdc5e4d82bb1beadc871
Davide Di Criscito <davide.dicriscito@gmail.com>
2023-02-05 18:28:26 +0100

    aggiusta le scadenze

--- server/src/main/java/me/ctrlmaniac/minigest/controllers/rest/FatturaScadenzaController.java
+++ server/src/main/java/me/ctrlmaniac/minigest/controllers/rest/FatturaScadenzaController.java
@@ -1,9 +1,15 @@
 package me.ctrlmaniac.minigest.controllers.rest;
 
+import me.ctrlmaniac.minigest.entitities.Account;
+import me.ctrlmaniac.minigest.entitities.azienda.Azienda;
+import me.ctrlmaniac.minigest.entitities.docfisc.fattura.Fattura;
 import me.ctrlmaniac.minigest.entitities.docfisc.fattura.FatturaScadenza;
+import me.ctrlmaniac.minigest.services.AccountService;
 import me.ctrlmaniac.minigest.services.docfisc.fattura.FatturaScadenzaService;
+import me.ctrlmaniac.minigest.services.docfisc.fattura.FatturaService;
 
 import java.util.List;
+import java.security.Principal;
 import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,17 +26,50 @@ public class FatturaScadenzaController {
 	@Autowired
 	FatturaScadenzaService fatturaScadenzaService;
 
-	@GetMapping("")
-	public ResponseEntity<List<FatturaScadenza>> getAll() {
-		List<FatturaScadenza> scadenze = new ArrayList<>();
+	@Autowired
+	AccountService accountService;
 
-		for (FatturaScadenza s : fatturaScadenzaService.getAll()) {
-			if (!s.getPagato()) {
-				scadenze.add(s);
+	@Autowired
+	FatturaService fatturaService;
+
+	@GetMapping("")
+	public ResponseEntity<?> getAll(Principal principal) {
+		Account user = accountService.findByEmail(principal.getName());
+
+		if (user == null) {
+			return new ResponseEntity<String>("Utente non connesso", HttpStatus.NOT_FOUND);
+		} else {
+			List<Azienda> aziende = user.getAziende();
+
+			if (aziende.size() == 0) {
+				return new ResponseEntity<String>("Non ci sono aziende per questo account", HttpStatus.NOT_FOUND);
+			} else {
+				List<Fattura> fatture = new ArrayList<>();
+
+				for (Azienda azienda : aziende) {
+					fatture.addAll(fatturaService.getAllByCommittente(azienda.getId()));
+				}
+
+				if (fatture.size() == 0) {
+					return new ResponseEntity<String>("Non ci sono fatture con scadenze", HttpStatus.NOT_FOUND);
+				} else {
+					List<FatturaScadenza> scadenzeAll = new ArrayList<>();
+					List<FatturaScadenza> scadenze = new ArrayList<>();
+
+					for (Fattura fattura : fatture) {
+						scadenzeAll.addAll(fattura.getScadenze());
+					}
+
+					for (FatturaScadenza scadenza : scadenzeAll) {
+						if (!scadenza.getPagato()) {
+							scadenze.add(scadenza);
+						}
+					}
+
+					return new ResponseEntity<>(scadenze, HttpStatus.OK);
+				}
 			}
 		}
-
-		return new ResponseEntity<>(scadenze, HttpStatus.OK);
 	}
 
 }
