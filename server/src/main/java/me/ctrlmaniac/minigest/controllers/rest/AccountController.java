826c1bae665940bd712d867388480ddac4899c83
Davide Di Criscito <davide.dicriscito@gmail.com>
2023-02-05 17:33:46 +0100

    sposta il form di signup in accountController

--- server/src/main/java/me/ctrlmaniac/minigest/controllers/rest/AccountController.java
+++ server/src/main/java/me/ctrlmaniac/minigest/controllers/rest/AccountController.java
@@ -1,6 +1,8 @@
 package me.ctrlmaniac.minigest.controllers.rest;
 
 import java.security.Principal;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -12,9 +14,15 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import me.ctrlmaniac.minigest.dto.FormRegistrazioneDTO;
 import me.ctrlmaniac.minigest.entitities.Account;
-import me.ctrlmaniac.minigest.repositories.AccountRepo;
+import me.ctrlmaniac.minigest.entitities.Negozio;
+import me.ctrlmaniac.minigest.entitities.azienda.Azienda;
+import me.ctrlmaniac.minigest.entitities.azienda.AziendaIndirizzo;
 import me.ctrlmaniac.minigest.services.AccountService;
+import me.ctrlmaniac.minigest.services.NegozioService;
+import me.ctrlmaniac.minigest.services.azienda.AziendaIndirizzoService;
+import me.ctrlmaniac.minigest.services.azienda.AziendaService;
 
 @RestController
 @RequestMapping("/api/account")
@@ -27,11 +35,17 @@ public class AccountController {
 	PasswordEncoder passwordEncoder;
 
 	@Autowired
-	AccountRepo accountRepo;
+	AziendaIndirizzoService aziendaIndirizzoService;
+
+	@Autowired
+	AziendaService aziendaService;
+
+	@Autowired
+	NegozioService negozioService;
 
 	@GetMapping("")
 	public ResponseEntity<?> currentUser(Principal principal) {
-		Account user = accountRepo.findByEmail(principal.getName());
+		Account user = accountService.findByEmail(principal.getName());
 
 		if (user == null) {
 			return new ResponseEntity<String>("Utente non connesso", HttpStatus.NOT_FOUND);
@@ -40,14 +54,60 @@ public class AccountController {
 		}
 	}
 
-	@PostMapping("/nuovo")
-	public ResponseEntity<Account> register(@RequestBody Account account) {
+	@PostMapping("")
+	public ResponseEntity<String> register(@RequestBody FormRegistrazioneDTO form) {
+		if (accountService.findByEmail(form.getAccount().getEmail()) != null) {
+			return new ResponseEntity<>("email già registrata", HttpStatus.CONFLICT);
+		} else {
+			// Salva l'indirizzo
+			AziendaIndirizzo sedeTmp = new AziendaIndirizzo();
+			sedeTmp.setIndirizzo(form.getSede().getIndirizzo());
+			sedeTmp.setCap(form.getSede().getCap());
+			sedeTmp.setComune(form.getSede().getComune());
+			sedeTmp.setNazione(form.getSede().getNazione());
+			sedeTmp.setNumeroCivico(form.getSede().getNumeroCivico());
+			sedeTmp.setProvincia(form.getSede().getProvincia());
 
-		String hashPwd = passwordEncoder.encode(account.getPassword());
-		account.setPassword(hashPwd);
-		Account saved = accountService.save(account);
+			AziendaIndirizzo sede = aziendaIndirizzoService.save(sedeTmp);
 
-		return new ResponseEntity<Account>(saved, HttpStatus.CREATED);
+			// Salva l'azienda
+			Azienda aziendaTmp = new Azienda();
+			aziendaTmp.setDenominazione(form.getAzienda().getDenominazione());
+			aziendaTmp.setIdFiscaleIVAPaese(form.getAzienda().getIdFiscaleIVAPaese());
+			aziendaTmp.setIdFiscaleIVACodice(form.getAzienda().getIdFiscaleIVACodice());
+			aziendaTmp.setCodiceFiscale(form.getAzienda().getCodiceFiscale());
+			aziendaTmp.setSede(sede);
+
+			Azienda azienda = aziendaService.save(aziendaTmp);
+
+			// Crea il negozio
+			Negozio negozio = new Negozio();
+			negozio.setAzienda(azienda);
+			negozio.setNome(form.getNegozio().getNome());
+			negozioService.save(negozio);
+
+			// Crea l'utente
+			String hashPwd = passwordEncoder.encode(form.getAccount().getPassword());
+
+			Account accountTmp = new Account();
+			accountTmp.setFname(form.getAccount().getFname());
+			accountTmp.setLname(form.getAccount().getLname());
+			accountTmp.setEmail(form.getAccount().getEmail());
+			accountTmp.setPassword(hashPwd);
+
+			List<Azienda> accountAziende = new ArrayList<>();
+			accountAziende.add(azienda);
+
+			accountTmp.setAziende(accountAziende);
+
+			Account account = accountService.save(accountTmp);
+
+			if (account.getId() != null) {
+				return new ResponseEntity<>("Utente creato con successo", HttpStatus.CREATED);
+			} else {
+				return new ResponseEntity<>("Errore creazione utente", HttpStatus.BAD_REQUEST);
+			}
+		}
 	}
 
 }
