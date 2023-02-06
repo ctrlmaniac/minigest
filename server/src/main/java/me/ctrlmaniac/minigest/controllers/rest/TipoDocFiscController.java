2ae0912df6ed342dfc39a576f6cc4b7f27689bd3
Davide Di Criscito <davide.dicriscito@gmail.com>
2023-02-06 21:47:14 +0100

    aggiunge preAuthorize

--- server/src/main/java/me/ctrlmaniac/minigest/controllers/rest/TipoDocFiscController.java
+++ server/src/main/java/me/ctrlmaniac/minigest/controllers/rest/TipoDocFiscController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -38,18 +39,21 @@ public class TipoDocFiscController {
 	}
 
 	@PostMapping("")
+	@PreAuthorize("hasRole('ADMIN')")
 	public ResponseEntity<TipoDocFisc> create(@RequestBody TipoDocFisc tdf) {
 		tdfService.save(tdf);
 		return new ResponseEntity<TipoDocFisc>(tdf, HttpStatus.CREATED);
 	}
 
 	@DeleteMapping("/{id}")
+	@PreAuthorize("hasRole('ADMIN')")
 	public ResponseEntity<String> deleteById(@PathVariable String id) {
 		tdfService.deleteById(id);
 		return new ResponseEntity<>("Tipo Documento Fiscale cancellato con successo!", HttpStatus.OK);
 	}
 
 	@PutMapping("/{id}")
+	@PreAuthorize("hasRole('ADMIN')")
 	public ResponseEntity<TipoDocFisc> update(@PathVariable String id, @RequestBody TipoDocFisc tdf) {
 		return new ResponseEntity<>(tdfService.update(id, tdf), HttpStatus.OK);
 	}
