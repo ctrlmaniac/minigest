312df03f491fab0381dae92eef09605b6c55362b
Davide Di Criscito <davide.dicriscito@gmail.com>
2023-02-06 15:24:51 +0100

    sposta tutto in www

--- www/vite.config.ts
+++ www/vite.config.ts
@@ -2,19 +2,46 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-// https://vitejs.dev/config/
-export default defineConfig({
+const commonConfigs = {
   plugins: [react()],
   resolve: {
     alias: {
       "~": path.resolve(__dirname, "src"),
     },
   },
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://127.0.0.1:8080",
+};
+
+// https://vitejs.dev/config/
+export default defineConfig(({ command }) => {
+  if (command === "serve") {
+    return {
+      ...commonConfigs,
+      server: {
+        proxy: {
+          "/api": {
+            target: "http://127.0.0.1:8080",
+          },
+        },
       },
-    },
-  },
+    };
+  } else {
+    return {
+      ...commonConfigs,
+      build: {
+        rollupOptions: {
+          output: {
+            assetFileNames: (assetInfo) => {
+              let extType = assetInfo.name.split(".").at(1);
+              if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+                extType = "img";
+              }
+              return `www/assets/${extType}/[name]-[hash][extname]`;
+            },
+            chunkFileNames: "www/assets/js/[name]-[hash].js",
+            entryFileNames: "www/assets/js/[name]-[hash].js",
+          },
+        },
+      },
+    };
+  }
 });
