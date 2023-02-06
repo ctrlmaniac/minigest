2a1b4526fe1831a45c27c64e0e667e2f2178afe6
Davide Di Criscito <davide.dicriscito@gmail.com>
2023-02-06 17:45:55 +0100

    collected clients

---
+++ clients/app/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import { Splash } from "components";
+
+function App() {
+  return (
+    <>
+      <Splash>
+        <h1>App</h1>
+      </Splash>
+    </>
+  );
+}
+
+export default App;
