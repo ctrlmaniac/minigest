1591783591734c0fc633869a474a6b35ed9efc87
Davide Di Criscito <davide.dicriscito@gmail.com>
2023-02-07 23:28:29 +0100

    init server

---
+++ server/src/test/java/me/ctrlmaniac/minigest/MinigestApplicationTests.java
@@ -0,0 +1,13 @@
+package me.ctrlmaniac.minigest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinigestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
