59809f239286d2af9206270648ab1e87c301c3f3
Davide Di Criscito <davide.dicriscito@gmail.com>
2023-02-12 21:44:22 +0100

    aggiunge la possibilità di salvare nel localstorage selectedAzienda e selectedNegozio

--- clients/app/src/pages/Root/index.tsx
+++ clients/app/src/pages/Root/index.tsx
@@ -2,13 +2,16 @@ import React from "react";
 import { useAppDispatch, useAppSelector } from "~/hooks";
 import { default as getPrincipal } from "~/features/account/get";
 import { ErrorScreen, LoadingScreen } from "components";
-import { find, isEmpty } from "lodash";
+import { findIndex, isEmpty } from "lodash";
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
 import Sidebar from "./Sidebar";
 import AziendeDialog from "./AziendeDialog";
 import NegoziDialog from "./NegoziDialog";
 import { Box, Container } from "@mui/material";
+import { setSelected as setSelectedAzienda } from "~/features/aziende/slice";
+import { setSelected as setSelectedNegozio } from "~/features/negozi/slice";
+import { Azienda, Negozio } from "~/types";
 
 const Root: React.FC = () => {
   const dispatch = useAppDispatch();
@@ -17,6 +20,9 @@ const Root: React.FC = () => {
     getError,
     dettagli: account,
   } = useAppSelector((state) => state.account);
+  const { selected: selectedAzienda } = useAppSelector(
+    (state) => state.aziende
+  );
 
   const [openSidebar, setOpenSidebar] = React.useState(false);
   const [openAziendeDialog, setOpenAziendeDialog] = React.useState(false);
@@ -25,6 +31,60 @@ const Root: React.FC = () => {
   React.useEffect(() => {
     dispatch(getPrincipal());
   }, []);
+
+  // Seleziona l'azienda
+  React.useEffect(() => {
+    if (!isEmpty(account)) {
+      const { aziende } = account;
+
+      const azienda = window.localStorage.getItem("azienda");
+
+      if (!isEmpty(azienda)) {
+        const index = findIndex(aziende, (o) => {
+          const parsed = JSON.parse(azienda!) as Azienda;
+          return o.id === parsed.id;
+        });
+
+        if (index > 1) {
+          dispatch(setSelectedAzienda(JSON.parse(azienda!)));
+        } else {
+          window.localStorage.removeItem("azienda");
+        }
+      } else {
+        if (!isEmpty(aziende)) {
+          dispatch(setSelectedAzienda(aziende![0]));
+          window.localStorage.setItem("azienda", JSON.stringify(aziende![0]));
+        }
+      }
+    }
+  }, [account]);
+
+  React.useEffect(() => {
+    const negozio = window.localStorage.getItem("negozio");
+
+    if (!isEmpty(selectedAzienda)) {
+      if (!isEmpty(negozio)) {
+        const index = findIndex(selectedAzienda?.negozi, (o) => {
+          const parsed = JSON.parse(negozio!) as Negozio;
+          return o.id === parsed.id;
+        });
+
+        if (index > 0) {
+          dispatch(setSelectedNegozio(JSON.parse(negozio!)));
+        } else {
+          window.localStorage.removeItem("negozio");
+        }
+      } else {
+        if (!isEmpty(selectedAzienda.negozi)) {
+          dispatch(setSelectedNegozio(selectedAzienda.negozi![0]));
+          window.localStorage.setItem(
+            "negozio",
+            JSON.stringify(selectedAzienda.negozi![0])
+          );
+        }
+      }
+    }
+  }, [selectedAzienda]);
 
   if (getting) {
     return <LoadingScreen />;
