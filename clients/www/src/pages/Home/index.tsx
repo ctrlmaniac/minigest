9b03ca8b2aa5febf9cf6d960bd3f47d7f99e1985
Davide Di Criscito <davide.dicriscito@gmail.com>
2023-02-17 21:57:22 +0100

    completa la home

--- clients/www/src/pages/Home/index.tsx
+++ clients/www/src/pages/Home/index.tsx
@@ -1,8 +1,11 @@
 import React from "react";
-import { Box, Button, Typography } from "@mui/material";
+import { Box, Button, Paper, Typography } from "@mui/material";
 import { Wallpaper } from "components";
 import dashboard from "./dashboard.webp";
 import corrispettivi from "./corrispettivi.webp";
+import fatture from "./fatture.webp";
+import scadenzario from "./scadenzario.webp";
+import bilancio from "./bilancio.webp";
 
 const Home: React.FC = () => {
   return (
@@ -25,10 +28,60 @@ const Home: React.FC = () => {
 
       <Box mt={6}>
         <Typography variant="h6" component="p" align="center" gutterBottom>
+          Gestisci le tue fatture, dai un'occhiata alle ultime fatture acquisto!
+        </Typography>
+        <Wallpaper src={fatture} alt="fatture" />
+      </Box>
+
+      <Box mt={6}>
+        <Typography variant="h6" component="p" align="center" gutterBottom>
           Gestisci le tue chiusure fiscali in base al negozio in cui sono state
           emesse!
         </Typography>
         <Wallpaper src={corrispettivi} alt="corrispettivi" />
+      </Box>
+
+      <Box mt={6}>
+        <Typography variant="h6" component="p" align="center" gutterBottom>
+          Tieni sempre d'occhio le tue scadenze!
+        </Typography>
+        <Wallpaper src={scadenzario} alt="scadenzario" />
+      </Box>
+
+      <Box mt={6}>
+        <Typography variant="h6" component="p" align="center" gutterBottom>
+          Controlla il bilancio delle entrate e delle uscite!
+        </Typography>
+        <Wallpaper src={bilancio} alt="bilancio" />
+      </Box>
+
+      <Box mt={6}>
+        <Paper>
+          <Box p={2}>
+            <Typography align="center">
+              Quest'applicazione è solo dimostrativa, sebbene ci si possa
+              registrare, non va utilizzata per salvare i vostri dati personali!
+            </Typography>
+            <Typography align="center">
+              I dati possono essere cancellati in qualsiasi momento, soprattuto
+              al riavvio dell'applicazione!
+            </Typography>
+
+            <Box sx={{ textAlign: "center" }} mt={3}>
+              <Button
+                variant="contained"
+                onClick={() =>
+                  window.open(
+                    "https://github.com/ctrlmaniac/minigest",
+                    "_blank"
+                  )
+                }
+              >
+                Visualizza il codice sorgente!
+              </Button>
+            </Box>
+          </Box>
+        </Paper>
       </Box>
     </>
   );
