58b9bf02e67ae61fcc76b2515c21e95f2c99dcc7
Davide Di Criscito <davide.dicriscito@gmail.com>
2023-03-14 09:34:11 +0100

    installa redux

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,14 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { RouterProvider } from "react-router-dom";
+import { Provider } from "react-redux";
 import { theme } from "./theme";
+import { store } from "./store";
 import routes from "./routes";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <RouterProvider router={routes} />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <RouterProvider router={routes} />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>
 );
